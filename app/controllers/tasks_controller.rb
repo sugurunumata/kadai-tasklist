7e0f6902dfbf9b5abdeef78a54ea8e6ce2530a9e
suguru numata <sn19920219@gmail.com>
2017-04-19 05:45:14 +0000

    create and destroy done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,48 +1,66 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all.page(params[:page]).per(3)
-  end
-  def show
-    @task = Task.find(params[:id])
-  end
-  def new
-    @task = Task.new
-  end
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+#  def index
+#    @tasks = Task.all.page(params[:page]).per(3)
+#  end
+#  def show
+#    @task = Task.find(params[:id])
+#  end
+#  def new
+#    @task = Task.new
+#  end
   def create
-    @task = Task.new(task_params)
+#    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'taskが正常に登録されました'
-      redirect_to @task
+#      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'taskが登録できません'
-      render :new
+      #render :new
+      render 'toppages/index'
     end
   end
-  def edit
-    @task = Task.find(params[:id])
-  end
-  def update
-    @task = Task.find(params[:id])
-    
-    if @task.update(task_params)
-      flash[:success] = 'taskは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'taskは更新されませんでした'
-      render :edit
-    end
-  end
+#  def edit
+#    @task = Task.find(params[:id])
+#  end
+#  def update
+#    @task = Task.find(params[:id])
+#    
+#    if @task.update(task_params)
+#      flash[:success] = 'taskは正常に更新されました'
+#      redirect_to @task
+#    else
+#      flash.now[:danger] = 'taskは更新されませんでした'
+#      render :edit
+#    end
+#  end
+  
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+#    @task = Task.find(params[:id])
+#    @task.destroy
     
-    flash[:success] = 'taskは正常に削除されました'
-    redirect_to tasks_url
+#    flash[:success] = 'taskは正常に削除されました'
+#    redirect_to tasks_url
+    @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
-end
+  
+  private
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 
-private
-# Strong Parameter
-def task_params
-  params.require(:task).permit(:content, :status)
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419014236) do
+ActiveRecord::Schema.define(version: 20170419045037) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.text     "status",     limit: 65535
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
